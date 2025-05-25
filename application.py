310b01d9b6fa98bcdd6054850df2bee39590df45
mukul <mukulvyas19@gmail.com>
2025-05-25 16:52:32 +0530

    forest fire prediction project done

---
+++ application.py
@@ -0,0 +1,40 @@
+import pickle
+from flask import Flask,request,jsonify,render_template
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+application = Flask(__name__)
+app = application
+
+## Import the model and scaler pickle files
+ridge_model = pickle.load(open('models/ridge.pkl', 'rb'))
+standard_scaler = pickle.load(open('models/scaler.pkl', 'rb'))
+
+@app.route('/') # this is the home page
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method == 'POST':
+        Temperature = float(request.form.get('Temperature'))
+        RH = float(request.form.get('RH'))
+        Ws = float(request.form.get('Ws'))
+        Rain = float(request.form.get('Rain'))
+        FFMC = float(request.form.get('FFMC'))
+        DMC = float(request.form.get('DMC'))
+        ISI = float(request.form.get('ISI'))
+        classes = float(request.form.get('Classes'))
+        region = float(request.form.get('Region'))
+
+        new_data_scaled = standard_scaler.transform([[Temperature, RH, Ws, Rain, FFMC, DMC, ISI, classes, region]])
+        prediction = ridge_model.predict(new_data_scaled)
+        
+        return render_template('home.html',results = prediction[0])
+
+    else:
+        return render_template('home.html')
+
+if __name__ == "__main__":
+    application.run(host="0.0.0.0")
